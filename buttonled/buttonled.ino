a369ebd2691c1ed2567c7a3aec36273c3745bed8
Hoai Nhi <nhicute20087@gmail.com>
2021-09-29 19:39:07 +0700

    Create buttonled.ino

---
+++ buttonled/buttonled.ino
@@ -0,0 +1,22 @@
+// C++ code
+//
+int x=0;
+void setup()
+{
+  pinMode(2, INPUT);
+  pinMode(13, OUTPUT);
+
+  
+}
+
+void loop()
+{
+  x = digitalRead(2);
+  if (x== HIGH){
+    
+    digitalWrite(13, HIGH);}
+  else
+  {
+    digitalWrite(13, LOW);
+
+  }}
